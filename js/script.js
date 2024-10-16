bdc1e7e9efd75724aefece16284771cd19835bd0
KaiqueBifulgo <kaiquebwork@hotmail.com>
2024-10-16 19:31:15 -0300

    Commit inicial

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// Seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+
+// FUNÇÕES
+const getLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+const getUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+    const symbols = "!@#$%&*()-=?|][*-/<>;:"
+    return symbols[Math.floor(Math.random () * symbols.length)];
+}
+
+const generatePassword = (getLowerCase, getUpperCase, getNumber, getSymbol) => {
+    let password = "";
+
+    const passwordLenght = 10;
+
+    const generators = [
+        getLowerCase,
+        getUpperCase,
+        getNumber,
+        getSymbol
+    ]
+
+    for (i = 0; i < passwordLenght; i = i + 4) {
+        generators.forEach(() => {
+            const randomValue = generators[Math.floor(Math.random () * generators.length)]();
+
+            password += randomValue;
+        })
+    }
+
+    password = password.slice(0, passwordLenght);
+
+    generatedPasswordElement.style.display = "block";
+    generatedPasswordElement.querySelector("h4").innerText = password;
+}
+
+
+// EVENTOS
+generatePasswordButton.addEventListener("click", () => {
+    generatePassword (
+        getLowerCase,
+        getUpperCase,
+        getNumber,
+        getSymbol
+    )
+})
